e6ab7520b6dbdd9786e99ed56b85b74be425b864
Huang-CL <huangclpku@gmail.com>
2021-10-29 00:44:37 -0700

    Adding plots to show the Roche Lobe property along the y axis.

--- RL.py
+++ RL.py
@@ -1,10 +1,11 @@
 import numpy as np
 from scipy import io as spio
 from scipy import optimize
-from numpy import vectorize
+from numpy import vectorize, interp
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib import rc
+#from matplotlib.ticker import MaxNLocator
 
 rc('text',usetex=True)          
 mpl.rcParams['font.size']=16.5
@@ -32,6 +33,7 @@ def pot3D(r, theta, phi, q, a):
 # three dimensional potential.  Substellar point theta = pi/2, phi = pi
   r1 = np.linalg.norm(np.array([a,0,0]) + np.array([r*np.sin(theta)*np.cos(phi),r*np.sin(theta)*np.sin(phi),r*np.cos(theta)]))
   R = np.linalg.norm(np.array([a*1/(1+q),0]) + np.array([r*np.sin(theta)*np.cos(phi),r*np.sin(theta)*np.sin(phi)]))
+#  print(r, theta, phi, q, a, r1, R, np.array([a,0,0]) + np.array([r*np.sin(theta)*np.cos(phi),r*np.sin(theta)*np.sin(phi),r*np.cos(theta)]), -(R**2*(1+q)/(2*a**3) + 1/r1 + q/r))
   return -(R**2*(1+q)/(2*a**3) + 1/r1 + q/r)
 
 def equiP(r, *args):
@@ -52,13 +54,40 @@ def getL(q, a):
   L1z = getequir(0, np.pi/2, L1x, np.pi/2, np.pi, L1x, q, a)
   return L1x,L1y,L1z
 
+vgetL = np.vectorize(getL)
 qlist = np.logspace(-5,0,50)
-Lx = np.zeros(50)
-Ly = np.zeros(50)
-Lz = np.zeros(50)
+Lx,Ly,Lz = vgetL(qlist,1)
 
-for i in range(len(qlist)):
-  Lx[i],Ly[i],Lz[i]=getL(qlist[i],1)
+def funcargs(r, *args):
+  func = lambda x,q:pot3D(x, np.pi/2, np.pi/2, q, 1)
+  if (r>0):
+    return -func(r[0], args[0])
+  else:
+    return -func(-r[0], args[0])
+
+def minPy(q,Lx):
+  return optimize.fmin(funcargs,Lx,args=(q,),disp=0)
+
+vminPy = np.vectorize(minPy)
+
+L4 = np.fabs(vminPy(qlist,Lx))
+
+q_mesh = 1E-4
+boxsize = 5*np.interp(q_mesh,qlist,Lx)
+x = np.linspace(-boxsize,boxsize,100)
+y = np.linspace(0,2*boxsize,100)
+X,Y = np.meshgrid(x,y)
+r_mesh = np.sqrt(X**2+Y**2)
+phi_mesh = np.arccos(X/r_mesh)
+
+vpot3D = np.vectorize(pot3D, excluded=['theta','q','a'])
+Z = vpot3D(r_mesh,np.pi/2.,phi_mesh,q_mesh,1)
+#levels = MaxNLocator(nbins=15).tick_values(Z.min(), Z.max())
+
+# ry = np.linspace(q_mesh,2*boxsize,100)
+# poty = vpot3D(ry,np.pi/2., np.pi/2., q_mesh,1)
+
+#print(np.transpose([ry,poty]))
 
 plt.plot(qlist, Lx)
 plt.xscale('log')
@@ -69,10 +98,23 @@ plt.ylabel('L1/a',fontsize=16)
 plt.figure()
 plt.plot(qlist, Ly/Lx, 'k-', label='y/x')
 plt.plot(qlist, Lz/Lx, 'b-', label='z/x')
-#plt.plot(qlist, Lz/Ly, 'r-', label='z/y')
+plt.plot(qlist, Lx/L4, 'r-', label='Lx/L4')
 plt.xscale('log')
 plt.xlabel('M2/M1',fontsize=16)
 plt.ylabel('Ratio',fontsize=16)
 plt.legend(loc=0, frameon = False)
 
+plt.figure()
+plt.plot(qlist, L4, 'k-')
+plt.xscale('log')
+plt.xlabel('M2/M1',fontsize=16)
+plt.ylabel('distance to minimum P in y direction',fontsize=16)
+
+plt.figure()
+plt.contourf(X,Y,np.log(Z.max()-Z+1E-5))
+
+# plt.figure()
+# plt.plot(ry,poty)
+
+#print(optimize.fmin(funcargs,np.interp(q_mesh,qlist,Lx),args=(q_mesh,),disp=1,full_output=1))
 plt.show()
